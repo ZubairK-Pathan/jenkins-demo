08cdefff6268cb1cb8e75039b971717b441fb52b
Zubair Khan Pathan <pzubair580@gmail.com>
2026-02-12 14:23:08 +0530

    Enhance app.py with system info and logging
    Added system information, a multiply function, and logging.

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Hello from GitHub + Jenkins!")
+from datetime import datetime
+import platform
+
+print("Jenkins Python Automation")
+
+# system info
+print("System:", platform.system())
+
+# simple test
+def multiply(a, b):
+    return a * b
+
+assert multiply(3, 4) == 12
+print("Test passed")
+
+# logging
+with open("build_log.txt", "a") as f:
+    f.write("Build run at: " + str(datetime.now()) + "\n")
+
+print("Log updated")
+print("Execution Complete")
